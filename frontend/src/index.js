66fda808cf6c522b78b5cef36d2930a902cc7324
Italo Contreras <icontrerasp@uni.pe>
2023-05-08 18:18:55 -0500

    deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-window.$api = "http://localhost:8000/api"
-// window.$api = "/api"
+// window.$api = "http://localhost:8000/api"
+window.$api = "/api"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
